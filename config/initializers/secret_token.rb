62cb304dd1f3e6f433a891f28f67df2e2a0d676e
Bettina Steger <bettina_steger@gmx.at>
2011-12-09 11:04:59 +0100

    added new clean rails 3.1.3 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alarmproject::Application.config.secret_token = '0e47dcfac0bc3979314c9c599842e841ffd43b199fb971ed1299ff98a6077e8ad4bc7f8df5fc84d774cc7d8e5241b1eace88ac009570e4b3b7a3980016d97ed3'
