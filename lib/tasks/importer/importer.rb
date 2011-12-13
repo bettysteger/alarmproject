4a1789da46115ea046142974400786e899ea9acf
Bettina Steger <bettina_steger@gmx.at>
2011-12-13 14:30:30 +0100

    WIP importer

--- lib/tasks/importer/importer.rb
+++ lib/tasks/importer/importer.rb
@@ -19,7 +19,7 @@ class Importer
     scenario = Scenario.find_or_create_by(name: @scenario)
     variable = Variable.find_or_create_by(name: @variable)
 
-    start_values, start_year, multi, year = nil
+    start_values, start_year, end_year, multi, year, point = nil
     line_counter = 0
   
     File.open(@filepath).each do |line|
@@ -27,9 +27,11 @@ class Importer
         month = 1
         line_counter += 1
         start_year = get_start_year(line) if start_year == nil
+        end_year = get_end_year(line) if end_year == nil
         multi = get_multi(line) if multi == nil
+        point = create_point(line) if point == nil #|| point.is_a?(Point)
         
-        if point = create_point(line)
+        if create_point(line) 
           year = start_year
           start_values = true
           next
@@ -43,9 +45,10 @@ class Importer
               raise "Error in #{@filepath} on line #{line_counter}. Wrong number of months"
             end
             Value.create!(model: model, scenario: scenario, variable: variable, 
-                      point: point, year: year, month: month, number: number)
+                     point: point, year: year, month: month, number: number)
             month += 1
           end
+          point = nil if year >= end_year # to generate a new point on the next grid_ref
           year += 1
         end
       rescue Exception => e
@@ -59,6 +62,11 @@ class Importer
     def get_start_year(line)
       result = line.match(/Years=(\d+)-(\d+)/)
       result ? result[1].to_i : nil
+    end
+    
+    def get_end_year(line)
+      result = line.match(/Years=(\d+)-(\d+)/)
+      result ? result[2].to_i : nil
     end
     
     def get_multi(line)
