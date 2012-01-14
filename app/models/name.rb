91dd263db0c1bca065ea2abbfb7eb4dc80e0b059
Matthias Frick <matthias@frick-web.at>
2012-01-14 15:47:24 +0100

    refactoring aggregate all function

--- app/models/name.rb
+++ app/models/name.rb
@@ -1,5 +1,5 @@
 class Name
-  def self.find_by_name name
+  def self.find_id_by_name name
     object = self.only(:id).where(name: name)
     raise "Error: #{self.name} nicht in der DB" unless object.first
     object.first.id
--- app/models/value.rb
+++ app/models/value.rb
@@ -20,9 +20,9 @@ class Value
   belongs_to :point, index: true
   
   def self.mapval_var params
-    model = Model.find_by_name(params[:model])
-    scenario = Scenario.find_by_name(params[:scenario])
-    var = Variable.find_by_name(params[:variable])
+    model = Model.find_id_by_name(params[:model])
+    scenario = Scenario.find_id_by_name(params[:scenario])
+    var = Variable.find_id_by_name(params[:variable])
     values = Value.where(model_id: model, scenario_id: scenario, variable_id: var, year: params[:year], month: params[:month])
     data = {}
     data[params[:variable]] = values.map(&:number)
@@ -30,8 +30,8 @@ class Value
   end
   
   def self.mapval_all params
-    model = Model.find_by_name(params[:model])
-    scenario = Scenario.find_by_name(params[:scenario])
+    model = Model.find_id_by_name(params[:model])
+    scenario = Scenario.find_id_by_name(params[:scenario])
     values = Value.where(model_id: model, scenario_id: scenario, year: params[:year], month: params[:month])
     data = {}
     Variable.all.each do |var|
@@ -41,67 +41,37 @@ class Value
   end
   
   def self.mapvalagr_var params
-    model = Model.find_by_name(params[:model])
-    scenario = Scenario.find_by_name(params[:scenario])
-    var = Variable.find_by_name(params[:variable])
+    model = Model.find_id_by_name(params[:model])
+    scenario = Scenario.find_id_by_name(params[:scenario])
+    var = Variable.find_id_by_name(params[:variable])
     
     data = {}
     data[params[:variable]] = []
     
-    #Value.all.max(:number)
-    #Value.all.min(:number)
-    #Value.all.avg(:number)
-    #raise wtf.inspect
-    
     values = Value.only(:number, :point_id).where(model_id: model, scenario_id: scenario, variable_id: var, year: params[:year])
-    #data[params[:variable]] = values.map_reduce(:number).last.number
-    #data = values.map_reduce(:number).map(&:number)
-    
     hash = values.asc(:number).group_by(&:point_id)
     hash.each do |k,v|
       data[params[:variable]] << v.first.number if params[:function] == "min"
       data[params[:variable]] << v.last.number if params[:function] == "max"
       data[params[:variable]] << v.collect(&:number).sum.to_f/v.length if params[:function] == "avg"
     end
-    
-    # map = "function(){ emit(new Date(this.created_at.getYear(), this.created_at.getMonth(), this.created_at.getDate()), {amount: this.amount}); };"
-    # reduce = "function(key, values){ var sum = 0; values.forEach(function(doc){ sum += doc.amount; }); return {amount: sum};};"
-    # 
-    # v = Value.collection.map_reduce(map, reduce).find.to_a
-    # raise v.inspect
-    
-    #values = Value.where(model_id: model, scenario_id: scenario, variable_id: var, year: params[:year])
-    #values.send(params[:function], :number)
-    #raise Value.all.group_by{ |v| v.point.to_hash}.inspect
-    #values.group_by(&:point).max(:number)
-    
-    # data = {}
-    # values = Value.where(model_id: model, scenario_id: scenario, variable_id: var, year: params[:year])#.group_by(&:point_id)
-    # points = values.collect(&:point)
-    # points.each do |p|
-    #   data[params[:variable]] = p.values.max(:number)
-    # end
-    #values.group(:point_id).max(:number)
-    # values.each do |k, v|
-    #   puts k
-    #   
-    # end 
-    # 
-    # raise values.inspect
-    
-    
-    #data = {}
-    #data[params[:variable]] = values #values.send(params[:function], :number, :group => :point_id)
     output_hash("val", params, data)
   end
   
   def self.mapvalagr_all params
-    model = Model.find_by_name(params[:model])
-    scenario = Scenario.find_by_name(params[:scenario])
-    values = Value.where(model_id: model, scenario_id: scenario, year: params[:year])
+    model = Model.find_id_by_name(params[:model])
+    scenario = Scenario.find_id_by_name(params[:scenario])
+    
     data = {}
     Variable.all.each do |var|
-      data[var.name] = values.where(variable_id: var.id).send(params[:function], :number)
+      data[var.name] = []
+      values = Value.only(:number, :point_id).where(model_id: model, scenario_id: scenario, variable_id: var.id, year: params[:year])
+      hash = values.asc(:number).group_by(&:point_id)
+      hash.each do |k,v|
+        data[var.name] << v.first.number if params[:function] == "min"
+        data[var.name] << v.last.number if params[:function] == "max"
+        data[var.name] << v.collect(&:number).sum.to_f/v.length if params[:function] == "avg"
+      end
     end
     output_hash("val", params, data)
   end
@@ -117,9 +87,4 @@ class Value
       data: data
     }
   end
-  
-  def self.wtf
-    Value.map_reduce(:number)#.first.number
-  end
-
 end
